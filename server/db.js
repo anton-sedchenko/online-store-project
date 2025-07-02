1c716344510e3cd88872343872a89ed52c871ae6
Anton Sedchenko <anton.sedchenko@gmail.com>
2025-07-02 22:26:30 +0300

    Admin panel fix

--- server/db.js
+++ server/db.js
@@ -1,12 +1,7 @@
 const {Sequelize} = require('sequelize');
 
-module.exports = new Sequelize(
-    process.env.DB_NAME, // Назва БД
-    process.env.DB_USER, // Користувач
-    process.env.DB_PASSWORD, // Пароль
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    }
-)
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false, // вимикає зайвий консоль лог
+});
