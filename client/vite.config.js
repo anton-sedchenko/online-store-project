8017eedbf7e03e0612cdfde3f7b94b7dfa44b9f7
Anton Sedchenko <anton.sedchenko@gmail.com>
2025-07-04 21:41:51 +0300

    clean vite config for vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,14 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'import.meta.env.VITE_APP_API_URL': JSON.stringify(env.VITE_APP_API_URL)
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_APP_API_URL,
-          changeOrigin: true,
-          secure: false,
-        }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://online-store-project-production.up.railway.app',
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
