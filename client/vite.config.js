db5ee180642607fd7fdeb60d2c4e906b2340c480
Anton Sedchenko <anton.sedchenko@gmail.com>
2025-07-04 18:42:21 +0300

    bug fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,21 @@
-import {defineConfig, loadEnv} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'import.meta.env.VITE_APP_API_URL': JSON.stringify(process.env.VITE_APP_API_URL)
-  },
-  scripts: {
-    "build": "echo 'Start build' && vite build && echo 'Build complete'"
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://online-store-project-production.up.railway.app',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env.VITE_APP_API_URL': JSON.stringify(env.VITE_APP_API_URL)
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_API_URL,
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
   }
-})
+});
