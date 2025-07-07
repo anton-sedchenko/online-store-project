b5b5ff91ac3d8e1278b8150793b987fb860a2f3e
Anton Sedchenko <anton.sedchenko@gmail.com>
2025-07-08 00:10:17 +0300

    pass reset bug fix

--- server/index.js
+++ server/index.js
@@ -58,7 +58,7 @@ const start = async () => {
     try {
         console.log("🔌 Using DB:", process.env.DATABASE_URL);
         await sequelize.authenticate();
-        await sequelize.sync({force: true});
+        // await sequelize.sync({alter: true});
 
         if (!PORT) throw new Error('PORT is not defined!');
         app.listen(PORT, () => {
