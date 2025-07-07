49347083d2fd9bfabb941abcf1e7d243a50806ba
Anton Sedchenko <anton.sedchenko@gmail.com>
2025-07-07 19:56:08 +0300

    logs added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,23 @@
 const Router = require('express');
 const router = new Router();
-const productRouter = require('./productRouter');
-const typeRouter = require('./typeRouter');
-const userRouter = require('./userRouter');
-const cartRouter = require('./cartRouter');
-const orderRouter = require('./orderRouter');
+
 
 try {
+    const productRouter = require('./productRouter');
+    const typeRouter = require('./typeRouter');
+    const userRouter = require('./userRouter');
+    const cartRouter = require('./cartRouter');
+    const orderRouter = require('./orderRouter');
+
+    console.log("🧭 Mounting user routes");
     router.use('/user', userRouter);
+    console.log("🧭 Mounting user routes");
     router.use('/type', typeRouter);
+    console.log("🧭 Mounting user routes");
     router.use('/product', productRouter);
+    console.log("🧭 Mounting user routes");
     router.use('/cart', cartRouter);
+    console.log("🧭 Mounting user routes");
     router.use('/order', orderRouter);
 } catch (e) {
     console.error("❌ ROUTER ERROR:", e);
