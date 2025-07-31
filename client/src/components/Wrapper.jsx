6fc9e86e928c550d028600546ca96eeb1ea9b798
Anton Sedchenko <anton.sedchenko@gmail.com>
2025-07-31 17:08:36 +0300

    added: banner about donation

--- client/src/components/Wrapper.jsx
+++ client/src/components/Wrapper.jsx
@@ -6,7 +6,14 @@ const Wrapper = ({children}) => {
     return (
         <div className="wrapper">
             <Header />
-            <div className="page__content">{children}</div>
+            <div className="page__content">
+                <div className="zsu__banner">
+                    <img src="https://res.cloudinary.com/dngcvygel/image/upload/v1753970699/%D0%B4%D0%BE%D0%BD%D0%B0%D1%82-1_eep1qf.png"
+                         alt="баннер 5% від замовлень перераховується на ЗСУ"
+                    />
+                </div>
+                {children}
+            </div>
             <Footer />
         </div>
     );
