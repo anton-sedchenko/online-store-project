95bdaf4bbf134f8b1b95f7dece519bca067c4b34
Anton Sedchenko <anton.sedchenko@gmail.com>
2025-07-31 17:47:25 +0300

    added: donation banner

--- client/src/components/Wrapper.jsx
+++ client/src/components/Wrapper.jsx
@@ -3,17 +3,22 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Banner from "./Banner.jsx";
 import {useLocation, useNavigate} from "react-router-dom";
-import {HOME_ROUTE, LOGIN_ROUTE} from "../utils/consts.js";
+import {DELIVERY_PAYMENT_ROUTE, HOME_ROUTE, LOGIN_ROUTE, OFERTA_ROUTE} from "../utils/consts.js";
 
 const Wrapper = ({children}) => {
     const location = useLocation();
-    const isGallery = location.pathname === HOME_ROUTE;
+    const bannerRoutes = [
+        HOME_ROUTE,
+        OFERTA_ROUTE,
+        DELIVERY_PAYMENT_ROUTE,
+    ];
+    const isBannerShow = bannerRoutes.includes(location.pathname);
 
     return (
         <div className="wrapper">
             <Header />
             <div className="page__content">
-                {(isGallery) && <Banner />}
+                {(isBannerShow) && <Banner />}
                 {children}
             </div>
             <Footer />
