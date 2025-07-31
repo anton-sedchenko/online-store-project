42af152ee7fa3cd9f4ce7537fcc73587fce501c7
Anton Sedchenko <anton.sedchenko@gmail.com>
2025-07-31 17:38:56 +0300

    fix:styles donation banner

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="zsu__banner__container">
+            <img src="https://res.cloudinary.com/dngcvygel/image/upload/v1753970699/%D0%B4%D0%BE%D0%BD%D0%B0%D1%82-1_eep1qf.png"
+                 alt="баннер 5% від замовлень перераховується на ЗСУ"
+            />
+        </div>
+    );
+};
+
+export default Banner;
--- client/src/components/Wrapper.jsx
+++ client/src/components/Wrapper.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import Banner from "./Banner.jsx";
+import {useLocation, useNavigate} from "react-router-dom";
+import {HOME_ROUTE, LOGIN_ROUTE} from "../utils/consts.js";
 
 const Wrapper = ({children}) => {
+    const location = useLocation();
+    const isGallery = location.pathname === HOME_ROUTE;
+
     return (
         <div className="wrapper">
             <Header />
             <div className="page__content">
-                <div className="zsu__banner">
-                    <img src="https://res.cloudinary.com/dngcvygel/image/upload/v1753970699/%D0%B4%D0%BE%D0%BD%D0%B0%D1%82-1_eep1qf.png"
-                         alt="баннер 5% від замовлень перераховується на ЗСУ"
-                    />
-                </div>
+                {(isGallery) && <Banner />}
                 {children}
             </div>
             <Footer />
