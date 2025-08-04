fcdb00ea8ccb78ae7060ce51f0f3fee9254df194
Anton Sedchenko <anton.sedchenko@gmail.com>
2025-08-04 21:03:42 +0300

    bugfix

--- client/src/pages/ArticlePage.jsx
+++ client/src/pages/ArticlePage.jsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import {fetchArticleBySlug} from '../http/articleAPI.js';
 import {Helmet} from "react-helmet-async";
+import {Col, Row} from "react-bootstrap";
+import SideBar from "../components/SideBar.jsx";
 
 const ArticlePage = () => {
     const {slug} = useParams();
@@ -19,10 +21,20 @@ const ArticlePage = () => {
                 <title>{article.title} – Charivna Craft</title>
                 <meta name="description" content={article.title} />
             </Helmet>
+
             <div className="component__container">
-                <h1>{article.title}</h1>
-                {article.image && <img src={article.image} alt="" className="img-fluid mb-4" />}
-                <div dangerouslySetInnerHTML={{__html: article.content}} />
+                <Row>
+                    <Col md={3} lg={2}>
+                        <SideBar/>
+                    </Col>
+                    <Col md={9} lg={10}>
+                        <h1>{article.title}</h1>
+                        <div className="article__img__container">
+                            {article.image && <img src={article.image} alt="" className="img-fluid mb-4" />}
+                        </div>
+                        <div dangerouslySetInnerHTML={{__html: article.content}} />
+                    </Col>
+                </Row>
             </div>
         </>
     );
