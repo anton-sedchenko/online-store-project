488e971ebb9c04e0f36b753e16c603b00a04419a
Anton Sedchenko <anton.sedchenko@gmail.com>
2026-04-11 16:25:25 +0300

    removed category page route and category page file

--- client/src/pages/CategoryPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { HOME_ROUTE } from '../utils/consts.js';
-
-const CategoryPage = () => {
-    return <Navigate to={HOME_ROUTE} replace />;
-};
-
-export default CategoryPage;
