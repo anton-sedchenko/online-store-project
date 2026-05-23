7afaf96676e15f4e5b1e0b268cb3c386841dea0e
Anton Sedchenko <anton.sedchenko@gmail.com>
2026-05-23 16:19:17 +0300

    adaptive styles fix

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import CartTable from "../components/CartTable.jsx";
 import SideBar from "../components/SideBar.jsx";
+import ProductsCounter from "../components/UI/ProductsCounter.jsx";
 import {Col, Row} from "react-bootstrap";
 import {Helmet} from "react-helmet-async";
+import {Link} from "react-router-dom";
+import {
+    BLOG_ROUTE,
+    CART_ROUTE,
+    CONTACTS_ROUTE,
+    HOME_ROUTE,
+    PROFILE_ROUTE
+} from "../utils/consts.js";
 
 const Cart = () => {
     return (
@@ -15,12 +24,24 @@ const Cart = () => {
                 />
             </Helmet>
 
-            <Row>
+            <nav className="cart__page__quick-nav" aria-label="Швидка навігація">
+                <Link to={HOME_ROUTE}>Каталог</Link>
+
+                <Link to={CART_ROUTE} className="cart__page__quick-nav__cart">
+                    <span>Мій кошик</span>
+                    <ProductsCounter />
+                </Link>
+
+                <Link to={BLOG_ROUTE}>Блог</Link>
+                <Link to={CONTACTS_ROUTE}>Контакти</Link>
+            </nav>
+
+            <Row className="cart__page__row">
                 <Col xl={2} className="cart__page__sidebar">
                     <SideBar/>
                 </Col>
 
-                <Col xl={10}>
+                <Col xl={10} className="cart__page__content">
                     <CartTable/>
                 </Col>
             </Row>
